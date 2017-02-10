5de3757d334ca4fe143d698e18cb607adc1de3b4
heini-phys-chem <heini.phys.chem@gmail.com>
2017-02-10 15:13:33 +0100

    got rid of system() warnings while compiling

--- src/mcGif.cpp
+++ src/mcGif.cpp
@@ -7,13 +7,22 @@ using namespace std;
 
 int mcGif(string mol_out)
 {
+    int ret_val1;
+    int ret_val2;
+    
     ostringstream oss;
     oss << "jmol " << mol_out << " -s gif/cmd.script --nodisplay";
     string cmd1 = oss.str(); 
     string cmd2 = "convert -delay 120 -loop 0 gif/all*.jpg gif/all.gif";
 
-    system(cmd1.c_str());
-    system(cmd2.c_str());
+    ret_val1 = system(cmd1.c_str());
+    ret_val2 = system(cmd2.c_str());
+
+    if (ret_val1 == 0 && ret_val2 == 0 && errno == 0) {
+        printf("successfully created fancy GIF!\n");
+    } else {
+        printf("where the fuck is my GIF?!?\n");
+    }
 
     return 0;
 }
