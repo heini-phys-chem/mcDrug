fb55ae376c38ca5ce202f34183e46870d31aacd7
Heini <heini.phys.chem@gmail.com>
2017-02-10 15:21:18 +0100

    Update mcGif.cpp

--- src/mcGif.cpp
+++ src/mcGif.cpp
@@ -15,5 +15,11 @@ int mcGif(string mol_out)
     system(cmd1.c_str());
     system(cmd2.c_str());
 
+    if (ret_val1 == 0 && ret_val2 == 0 && errno == 0) {
+        printf("successfully created fancy GIF!\n");
+    } else {
+        printf("where the fuck is my GIF?!?\n");
+    }
+
     return 0;
 }
