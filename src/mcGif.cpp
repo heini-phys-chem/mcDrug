6ba912779ece61c2823dd537c83b84ed468c2ff1
Heini <heini.phys.chem@gmail.com>
2017-02-10 15:22:11 +0100

    Update mcGif.cpp

--- src/mcGif.cpp
+++ src/mcGif.cpp
@@ -7,13 +7,16 @@ using namespace std;
 
 int mcGif(string mol_out)
 {
+    int ret_val1;
+    int ret_val2;
+    
     ostringstream oss;
     oss << "jmol " << mol_out << " -s gif/cmd.script --nodisplay";
     string cmd1 = oss.str(); 
     string cmd2 = "convert -delay 120 -loop 0 gif/all*.jpg gif/all.gif";
 
-    system(cmd1.c_str());
-    system(cmd2.c_str());
+    ret_val1 = system(cmd1.c_str());
+    ret_val2 = system(cmd2.c_str());
 
     if (ret_val1 == 0 && ret_val2 == 0 && errno == 0) {
         printf("successfully created fancy GIF!\n");
